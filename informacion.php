06c19e99675ef33085760725b15b05e1488cb705
Laura-Morales1w1 <lauramoralesleon064@gmail.com>
2024-08-06 14:58:05 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,9 @@
-
+<?php
+$Identificacion= $_POST ['Identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$cargo=$_POST ['Cargo'];
+echo "La identificacion del Empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado son: ".NomApelldios;
+echo "El cargo del empelado es: ".Cargo;
+echo "El salario del empleado es: ".Salario;
+?>
